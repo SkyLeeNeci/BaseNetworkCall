d1c9b9ab10741f7ba4fa3e700dbb697f29ba9168
Vladyslav Karpenko <vladyslav.karpenko@chisw.com>
2022-11-09 13:10:33 +0200

    replace threads with coroutines and made 3 tasks

--- app/src/main/java/test/karpenko/basenetworkcall/AnimalFragment.kt
+++ app/src/main/java/test/karpenko/basenetworkcall/AnimalFragment.kt
@@ -1,32 +1,25 @@
 package test.karpenko.basenetworkcall
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import okhttp3.*
-import retrofit2.Call
-import retrofit2.Response
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.*
 import test.karpenko.basenetworkcall.adapter.AnimalAdapter
 import test.karpenko.basenetworkcall.api.AnimalApiFactory
 import test.karpenko.basenetworkcall.databinding.FragmentAnimalBinding
 import test.karpenko.basenetworkcall.models.Animal
-import test.karpenko.basenetworkcall.utils.URL
 import java.io.IOException
+import java.lang.IllegalArgumentException
 
 
 class AnimalFragment : Fragment() {
-
-    private var retrofitResponse = mutableListOf<Animal>()
-    private var okHttpResponse = mutableListOf<Animal>()
-    private var summaryResponse = mutableListOf<Animal>()
 
     private lateinit var binding: FragmentAnimalBinding
     private lateinit var animalAdapter: AnimalAdapter
@@ -42,7 +35,7 @@ class AnimalFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initAdapter()
-        init()
+        init3()
     }
 
 
@@ -55,41 +48,90 @@ class AnimalFragment : Fragment() {
 
     }
 
-    private fun getOkHttpResponse(){
-        val client = OkHttpClient()
-        val request: Request = Request.Builder()
-            .url(URL)
-            .build()
-
-        val okhttps =  client.newCall(request).execute()
-        if (okhttps.isSuccessful){
-            val string = okhttps.body?.string()
-            val userType = object : TypeToken<MutableList<Animal>>() {}.type
-            okHttpResponse = (Gson().fromJson(string, userType))
-            summaryResponse.addAll(okHttpResponse)
-
-        }
-
-    }
-
-    private fun getRetrofitResponse()  {
-       val retrofitResponse1 = AnimalApiFactory.animalService.getRandomAnimalsCount("5").execute()
-        if (retrofitResponse1.isSuccessful){
-            retrofitResponse1.body()?.let { retrofitResponse = it }
-            summaryResponse.addAll(retrofitResponse)
-        }
-    }
-
-    fun init(){
-        Thread{
-            getRetrofitResponse()
-            getOkHttpResponse()
-            Log.d("RESPONSE", "retrofitResponse $summaryResponse")
-            Handler(Looper.getMainLooper()).post{
-                animalAdapter.differ.submitList(summaryResponse)
+    //Task 1
+    fun init1() {
+        lifecycleScope.launch{
+            val response = AnimalApiFactory.animalService.getRandomAnimalsCount("10")
+            if (response.isSuccessful){
+                animalAdapter.differ.submitList(response.body())
             }
-        }.start()
+        }
+    }
 
+    //Task2
+    fun init2() =
+        lifecycleScope.launch {
+            var i = 0
+            val listOfAnimals = mutableListOf<Animal>()
+            val job: Job = launch(Dispatchers.IO) {
+                while (i < 3) {
+                    i += 1
+                    val response = AnimalApiFactory.animalService.getRandomAnimalsCount("1")
+                    if (response.isSuccessful) {
+                        Log.d("MAIN_ACTIVITY", response.body().toString())
+                        response.body()?.let { listOfAnimals.addAll(it) }
+                    }else{
+                        Log.d("MAIN_ACTIVITY", response.message())
+                    }
+                    //Log.d("MAIN_ACTIVITY", "$i")
+                    delay( 500)
+                }
+            }
+            delay(1000)
+            Log.d("MAIN_ACTIVITY", "I'm tired of waiting!")
+            //job.join()
+            job.cancel()
+            //job.cancelAndJoin()
+            /*job.cancel()
+            job.join()*/
+            withContext(Dispatchers.Main){
+                animalAdapter.differ.submitList(listOfAnimals)
+            }
+            Log.d("MAIN_ACTIVITY", " Now I can quit.")
+        }
+
+    //Task3
+    fun init3(){
+
+        val superJob = SupervisorJob()
+        val job = Job()
+        val superScope = CoroutineScope(Dispatchers.Default + superJob)
+        val scope = CoroutineScope(Dispatchers.Default + job)
+
+        /*scope.launch {
+            Log.d("MAIN_ACTIVITY", " Now in launch")
+            try {
+                async {
+                    Log.d("MAIN_ACTIVITY", " Now in async")
+                    delay(1000)
+                    Log.d("MAIN_ACTIVITY", " Now in async after delay")
+                    throw Exception("Oops...")
+                }.await()
+            }catch (e: Exception){
+                withContext(Dispatchers.Main){
+                    Snackbar.make(binding.rootConstrain, e.message.toString(), Snackbar.LENGTH_LONG)
+                }
+            }
+            Log.d("MAIN_ACTIVITY", " Now in launch after async")
+        }*/
+
+        superScope.launch {
+            Log.d("MAIN_ACTIVITY", " Now in launch")
+            try {
+                async {
+                    Log.d("MAIN_ACTIVITY", " Now in async")
+                    delay(1000)
+                    Log.d("MAIN_ACTIVITY", " Now in async after delay")
+                    throw CancellationException("Oops...")
+                }.await()
+            }catch (e: Exception){
+                withContext(Dispatchers.Main){
+                    Snackbar.make(binding.rootConstrain, e.message.toString(), Snackbar.LENGTH_LONG).show()
+                }
+                Log.d("MAIN_ACTIVITY", " Now in launch after async")
+            }
+            Log.d("MAIN_ACTIVITY", " Now in launch after async")
+        }
 
     }
 
--- app/src/main/java/test/karpenko/basenetworkcall/api/AnimalService.kt
+++ app/src/main/java/test/karpenko/basenetworkcall/api/AnimalService.kt
@@ -1,6 +1,7 @@
 package test.karpenko.basenetworkcall.api
 
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import test.karpenko.basenetworkcall.models.Animal
@@ -8,8 +9,8 @@ import test.karpenko.basenetworkcall.models.Animal
 interface AnimalService {
 
     @GET("{count}")
-    fun getRandomAnimalsCount(
+    suspend fun getRandomAnimalsCount(
         @Path("count") count: String
-    ): Call<MutableList<Animal>>
+    ): Response<MutableList<Animal>>
 
 }
